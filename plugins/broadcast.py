6b16a8f9b888d9cee4da475b74606901411c650b
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-06-28 07:13:45 +0530

    Create broadcast.py

---
+++ plugins/broadcast.py
@@ -0,0 +1,106 @@
+import re
+import uuid
+import socket
+import platform
+import time
+import math
+import json
+import string
+import traceback
+import psutil
+import asyncio
+import wget
+import motor.motor_asyncio
+import pymongo
+import aiofiles
+import datetime
+import os
+import random
+import logging
+from pyrogram.errors.exceptions.bad_request_400 import *
+from pyrogram.errors import *
+from pyrogram.types import *
+from helper.decorators import humanbytes
+from asyncio import *
+import requests
+from utils.database import Database
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import *
+from pyrogram.types import Message
+
+from info import START_MSG, CHANNELS, ADMINS, INVITE_MSG, DATABASE_URI, PRIVATE_LOG
+from utils import Media, unpack_new_file_id
+
+logger = logging.getLogger(__name__)
+
+async def send_msg(user_id, message):
+    try:
+        await message.copy(chat_id=user_id)
+        return 200, None
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return send_msg(user_id, message)
+    except InputUserDeactivated:
+        return 400, f"{user_id} : deactivated\n"
+    except UserIsBlocked:
+        return 400, f"{user_id} : user is blocked\n"
+    except PeerIdInvalid:
+        return 400, f"{user_id} : user id invalid\n"
+    except Exception as e:
+        return 500, f"{user_id} : {traceback.format_exc()}\n"
+
+#=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•Epic Bots 2022© All Rights Resived•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=#
+DATABASE_URI=DATABASE_URI
+database = Database(DATABASE_URI, "epic_bot")  
+#=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•Epic Bots 2022© All Rights Resived•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=#
+   
+
+@Client.on_message(filters.incoming & filters.chat(-1001609244993))
+async def bchanl(bot, update, broadcast_ids={}): 
+    all_users = await database.get_all_users()
+    broadcast_msg= update
+    while True:
+        broadcast_id = ''.join([random.choice(string.ascii_letters) for i in range(3)])
+        if not broadcast_ids.get(broadcast_id):
+            break
+
+    out = await bot.send_message(-1001645328504,f"Ads Broadcast Started! You will be notified with log file when all the users are notified.")
+    start_time = time.time()
+    total_users = await database.total_users_count()
+    done = 0
+    failed = 0
+    success = 0
+    broadcast_ids[broadcast_id] = dict(total = total_users, current = done, failed = failed, success = success)
+        
+    async with aiofiles.open('broadcastlog.txt', 'w') as broadcast_log_file:
+        async for user in all_users:
+            sts, msg = await send_msg(user_id = int(user['id']), message = broadcast_msg)
+            if msg is not None:
+                await broadcast_log_file.write(msg)
+            if sts == 200:
+                success += 1
+            else:
+                failed += 1
+            if sts == 400:
+                await database.delete_user(user['id'])
+            done += 1
+            if broadcast_ids.get(broadcast_id) is None:
+                break
+            else:
+                broadcast_ids[broadcast_id].update(dict(current = done, failed = failed, success = success))
+        
+    if broadcast_ids.get(broadcast_id):
+        broadcast_ids.pop(broadcast_id)
+    
+    completed_in = datetime.timedelta(seconds=int(time.time()-start_time))
+    await asyncio.sleep(3)
+    await out.delete()
+    
+    if failed == 0:
+        await bot.send_message(-1001645328504, f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.")
+    else:
+        await bot.send_document(-1001645328504, 'broadcastlog.txt', caption=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.")
+    os.remove('broadcastlog.txt') 
+    
+        
