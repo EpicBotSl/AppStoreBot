aab0c1ecd77047deffcb86fc445f20cb4670c70c
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-06-29 10:00:58 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -79,7 +79,7 @@ def get_reply_markup(username, query):
     url = 't.me/share/url?url=' + quote(SHARE_BUTTON_TEXT.format(username=username))
     buttons = [
         [
-            InlineKeyboardButton('🔄Search again', switch_inline_query_current_chat=query),
+            InlineKeyboardButton('🔄Search again', switch_inline_query_current_chat=''),
             InlineKeyboardButton('🔥Share bot', url=url),
         ]
     ]
