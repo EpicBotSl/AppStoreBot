b3491e2813202509f5e9baf34a160ff8f497b953
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-06-30 17:47:13 +0530

    Update pm.py

--- plugins/pm.py
+++ plugins/pm.py
@@ -54,8 +54,7 @@ async def pm_text(bot, message):
         chat_id=LOG_CHANNEL,
         text=PM_TXT_ATT.format(reference_id, info.first_name, message.text)
     )
-    
-
+   
 @Client.on_message(filters.private & filters.sticker)
 async def pm_sticker(bot, message):
     if message.from_user.id == OWNER_ID:
@@ -78,62 +77,6 @@ async def pm_sticker(bot, message):
     )
     await bot.send_message(LOG_CHANNEL, text=PM_TXT_ATTS.format(reference_id, info.first_name))
     
-@Client.on_message(filters.private & filters.media)
-async def pm_media(bot, message):
-    if message.from_user.id == OWNER_ID:
-        await replay_media(bot, message)
-        return
-    if await forcesub(bot, message):
-       return
-    await message.reply_text(text=f"Ur Photo Sent To @{force_subchannel} Admins", reply_markup=CLOSE_BUTTON)
-    info = await bot.get_users(user_ids=message.from_user.id)
-    reference_id = int(message.chat.id)
-    msg=message.caption
-    await bot.copy_message(
-        chat_id=OWNER_ID,       
-        from_chat_id=message.chat.id,
-        message_id=message.id,
-        caption=PM_MED_ATT.format(reference_id, message.from_user.mention, msg)
-    )
-
-
-@Client.on_message(filters.text)
-async def reply_text(bot, message):
-    reference_id = True
-    if message.replay_to_message is not None:
-        file = message.replay_to_message
-        try:
-            reference_id = file.text.split()[2]
-        except Exception:
-            pass
-        try:
-            reference_id = file.caption.split()[2]
-        except Exception:
-            pass
-        await bot.send_message(
-            text=message.text,
-            chat_id=int(reference_id)
-        )
-
-
-@Client.on_message(filters.media)
-async def replay_media(bot, message):
-    reference_id = True
-    if message.reply_to_message is not None:
-        file = message.reply_to_message
-        try:
-            reference_id = file.text.split()[2]
-        except Exception:
-            pass
-        try:
-            reference_id = file.caption.split()[2]
-        except Exception:
-            pass
-        await bot.copy_message(
-            chat_id=int(reference_id),
-            from_chat_id=message.chat.id,
-            message_id=message.id
-        )
 #=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•Epic Bots 2022© All Rights Resived•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=#
 USER_DETAILS = "<b>PM FROM:</b>\nName: {} {}\nId: {}\nUname: @{}\nScam: {}\nRestricted: {}\nStatus: {}\nDc Id: {}"
 PM_TXT_ATT = "<b>Message from:</b> {}\n<b>Name:</b> {}\n\n{}"
