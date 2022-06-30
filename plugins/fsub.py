fdda8985950725ad13e2a749f409791e2ed872f7
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-06-30 09:41:24 +0530

    Create fsub.py

---
+++ plugins/fsub.py
@@ -0,0 +1,28 @@
+from pyrogram.types import *
+from pyrogram.errors import *
+from config import *
+from pyrogram import *
+from pyrogram import InlineKeyboardMarkup, InlineKeyboardButton 
+
+
+async def forcesub(bot, update):
+        try:
+            await bot.get_chat_member(force_subchannel, update.from_user.id)
+        except UserNotParticipant:
+            file_id = "CAACAgUAAxkBAAEFIihiuYjFehkzzJg6fBsp9NSddE2QSQACsAYAAseOyVXbaQF75owUgCkE"
+            await bot.send_sticker(update.from_user.id, file_id)
+            text = FORCESUB_TEXT
+            reply_markup = FORCESUB_BUTTONS
+            return await bot.send_message(update.from_user.id,
+            text=text,
+            reply_markup=reply_markup,
+            disable_web_page_preview=True) 
+
+FORCESUB_TEXT = "WTF Join Our Chanel humm 😒.."
+
+FORCESUB_BUTTONS = InlineKeyboardMarkup([
+[InlineKeyboardButton('Join Chanel🌱', url='https://t.me/EpicBotsSl)]
+])
+
+
+
